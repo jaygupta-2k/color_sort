b40b85d3f07c24fe1734d36f7fe30ededc9f653a
Jay Gupta <jaygupta.id@gmail.com>
2024-01-21 23:36:21 +0530

    Update letter_func.py

--- letter_func.py
+++ letter_func.py
@@ -4,7 +4,7 @@ def move_letters(orig, final):
     if len(final)<4 and len(orig)>0:
         if len(final)==0 or final[-1]==orig[-1]:
             prev_val = orig[-1]
-            while len(final)<5 and orig[-1]==prev_val:
+            while len(final)<4 and orig[-1]==prev_val:
                 prev_val=orig[-1]
                 final.append(orig.pop())
                 if len(orig)==0:
